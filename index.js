1bd031cf1fdb9bb8638b8c2660fcda0a5fc46256
Govind Singh <govind.s@enlume.com>
2024-08-22 14:38:14 +0530

    refactor: 💡 modify add func to support custom delimiters

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 export const add = (numbers) => {
-  if (numbers.trim() === "") {
+  let numberString = numbers.trim();
+
+  if (numberString === "") {
     return 0;
   }
 
-  const numbersArray = numbers.replace(/\n/g, ",").split(",");
-  const sum = numbersArray.reduce((total, number) => total + Number(number), 0);
+  let delimiter = ",";
+
+  if (numberString.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    delimiter = numberString.substring(2, delimiterEndIndex);
+    numberString = numberString.substring(delimiterEndIndex + 1);
+  }
+
+  const numbersArray = numberString.replace(/\n/g, delimiter).split(delimiter);
+  const sum = numbersArray.reduce(
+    (total, number) => total + (Number(number) || 0),
+    0
+  );
 
   return sum;
 };
