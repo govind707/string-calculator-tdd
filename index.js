d75d5cb9f481ad15280e696febe6735ee0a29502
Govind Singh <govind.s@enlume.com>
2024-08-22 13:36:44 +0530

    refactor: 💡 modify func add to support newlines between numbers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ export const add = (numbers) => {
     return 0;
   }
 
-  const numbersArray = numbers.split(",");
+  const numbersArray = numbers.replace(/\n/g, ",").split(",");
   const sum = numbersArray.reduce((total, number) => total + Number(number), 0);
 
   return sum;
