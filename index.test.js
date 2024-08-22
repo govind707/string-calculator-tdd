6fde684de48600dca64123369248524b565c7242
Govind Singh <govind.s@enlume.com>
2024-08-22 13:16:23 +0530

    test: 💍 add test case to return sum when two comma-separated no

--- index.test.js
+++ index.test.js
@@ -9,4 +9,8 @@ describe("String Calculator", () => {
   it("when one number is provided return the number itself", () => {
     expect(add("7")).to.equal(7);
   });
+
+  it("when two comma-separated numbers return the sum of numbers", () => {
+    expect(add("3,4")).to.equal(7);
+  });
 });
